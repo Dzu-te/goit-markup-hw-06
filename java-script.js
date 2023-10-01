63054dbbd764a5ff10e928cd55957b036009725b
Dzu-te <108475129+Dzu-te@users.noreply.github.com>
2023-10-02 00:51:09 +0300

    add JS to mobile menu

--- java-script.js
+++ java-script.js
@@ -11,3 +11,17 @@ document.addEventListener("DOMContentLoaded", function () {
         overLay.classList.remove("is-open")
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const openButtonMenu = document.getElementById("openButtonMenu");
+    const closeButtonMenu = document.getElementById("closeButtonMenu");
+    const overLayMenu = document.getElementById("overLayMenu");
+
+    openButtonMenu?.addEventListener("click", function () {
+        overLayMenu.classList.add("is-open")
+    });
+
+    closeButtonMenu?.addEventListener("click", function () {
+        overLayMenu.classList.remove("is-open")
+    });
+});
