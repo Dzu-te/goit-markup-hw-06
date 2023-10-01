d7138f07f9f5189cb170891a2ec368fd3bdac942
Dzu-te <108475129+Dzu-te@users.noreply.github.com>
2023-10-01 20:56:48 +0300

    1

--- java-script.js
+++ java-script.js
@@ -3,11 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const closeButton = document.getElementById("closeButton");
     const overLay = document.getElementById("overLay");
 
-    openButton.addEventListener("click", function () {
-        overLay.classList.add("is-hidden")
+    openButton?.addEventListener("click", function () {
+        overLay.classList.add("is-open")
     });
 
-    closeButton.addEventListener("click", function () {
-        overLay.classList.remove("is-hidden")
+    closeButton?.addEventListener("click", function () {
+        overLay.classList.remove("is-open")
     });
 });
